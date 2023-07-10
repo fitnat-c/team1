437972aaa0ed01b2453d44d9f75cf36a3284dcb3
Mustafa Aba <mustafa_aba@outlook.com>
2023-07-10 13:13:59 +0300

    m.aba

---
+++ src/MustafaAba/tasks10_Methods/Task09.java
@@ -0,0 +1,18 @@
+package MustafaAba.tasks10_Methods;
+
+public class Task09 {
+
+    public static void main(String[] args) {
+
+    /*
+    Saati saniyeye çeviren  method create ediniz
+
+    Test Data:
+    howManySeconds(2);
+    Beklenen çıktı:
+     7200
+    */
+        Task03.hourToSecond();
+    }
+
+}
