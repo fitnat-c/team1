4c88508ca94642bec01a7c96f0d94816cee56a8a
Mustafa Aba <mustafa_aba@outlook.com>
2023-07-10 12:42:00 +0300

    m.aba

---
+++ src/UcusProjesi/menu.java
@@ -0,0 +1,31 @@
+package UcusProjesi;
+
+import java.util.Scanner;
+
+public class menu {
+    static double ucret;
+
+    public static void giris(){
+        System.out.print("Hangi sehire gitmek istersiniz:  \t B\t C\t D");
+        Scanner input = new Scanner(System.in);
+        char sehir = input.next().toUpperCase().charAt(0);
+
+        System.out.print("Yasınızı giriniz: ");
+        int yas= input.nextInt();
+
+        System.out.print("gidisdonus istiyorsanız ' E ' giriniz: ");
+        char yon=input.next().toUpperCase().charAt(0);
+
+        ucret=secim.sehirSec(sehir);
+        ucret=secim.yasKontrol(yas);
+        ucret=secim.gidisDonus(yon);
+        secim.ucretYaz(ucret);
+
+    }
+
+
+
+
+
+
+}
---
+++ src/UcusProjesi/secim.java
@@ -0,0 +1,55 @@
+package UcusProjesi;
+
+public class secim {
+    static double toplamUcret;
+
+    public static double sehirSec(char sehir) {
+
+        switch (sehir) {
+            case 'B':
+                toplamUcret = ucretHesapla(500, 0.1);
+                break;
+            case 'C':
+                toplamUcret = ucretHesapla(700, 0.1);
+                break;
+            case 'D':
+                toplamUcret = ucretHesapla(900, 0.1);
+                break;
+            default:
+                System.out.println("Yanlış giriş yaptınız. Tekrar deneyin");
+                sehirSec(sehir);
+        }
+        return toplamUcret;
+
+    }
+    public static double yasKontrol(int yas){
+        if (yas>=0) {
+
+            if (yas<=12) {
+                toplamUcret=ucretHesapla(toplamUcret,0.5);
+            } else if (yas<=24) {
+                toplamUcret=ucretHesapla(toplamUcret,0.9);
+            } else if (yas>=65) {
+                toplamUcret=ucretHesapla(toplamUcret,0.7);
+            }
+        }else {
+            System.out.println("Yas negatif olamaz! tekar deneyin");
+            yasKontrol(yas);
+        }
+        return toplamUcret;
+    }
+
+    public static double gidisDonus(char ch){
+        if (ch=='E') {
+            toplamUcret=ucretHesapla(toplamUcret,0.8)*2;
+        }
+        return toplamUcret;
+    }
+    public static double ucretHesapla(double x,double y) {
+        return x*y;
+    }
+    public static void ucretYaz(double toplamUcret){
+        System.out.println("toplam ucret="+ toplamUcret);
+    }
+
+}
