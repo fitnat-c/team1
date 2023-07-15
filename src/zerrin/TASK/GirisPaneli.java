cc214c52a2365db889b52b79e6388b725058b5cb
zerrin <zerrinizgu@gmail.com>
2023-07-15 16:21:59 +0200

    13 temmuz TASK

--- src/zerrin/TASK/GirisPaneli.java
+++ src/zerrin/TASK/GirisPaneli.java
@@ -9,36 +9,34 @@ public class GirisPaneli {
 
     public void giris() {
 
-   KayitMetotlari obj=new KayitMetotlari();
-        Scanner input=new Scanner(System.in);
-        System.out.println("islemi sec"+"\n1->kayit ol\n2->sansli kisi bul"+"\n3->tum kullanicilari listele\n4->cikis");
+        KayitMetotlari obj = new KayitMetotlari();
+        Scanner input = new Scanner(System.in);
+        System.out.println("islemi sec" + "\n1->kayit ol\n2->sansli kisi bul" + "\n3->tum kullanicilari listele\n4->cikis");
 
-int secim=input.nextInt();
-switch (secim) {
-   case 1:
-    obj.kayitAl();
-    giris();
-    break;
-    case 2:
-        obj.sansliKullanici(kullaniciList);
-giris();
-break;
-case 3:
-obj.listele();
-giris();
-break;
-case 4:
-    obj.cikis();
-    break;
-    default:
-        System.out.println("yanlis tuslama yaptin tekrar dene");
-giris();
-break;
-
-
+        int secim = input.nextInt();
+        switch (secim) {
+            case 1:
+                obj.kayitAl();
+                giris();
+                break;
+            case 2:
+                obj.sansliKullanici(kullaniciList);
+                giris();
+                break;
+            case 3:
+                obj.listele();
+                giris();
+                break;
+            case 4:
+                obj.cikis();
+                break;
+            default:
+                System.out.println("yanlis tuslama yaptin tekrar dene");
+                giris();
+                break;
 
 
-}
+        }
 
     }
 
--- src/zerrin/TASK/KayitMetotlari.java
+++ src/zerrin/TASK/KayitMetotlari.java
@@ -6,36 +6,39 @@ import java.util.Scanner;
 
 public class KayitMetotlari {
 
-    static ArrayList<Kullanici>kullaniciList = new ArrayList<>();
+    static ArrayList<Kullanici> kullaniciList = new ArrayList<>();
     static Scanner input = new Scanner(System.in);
 
-    public  ArrayList<Kullanici>kayitAl() {
+    public ArrayList<Kullanici> kayitAl() {
         System.out.println("ismini gir");
-String isim=input.nextLine();
-      //  LocalDateTime time=LocalDateTime.now();
+        String isim = input.nextLine();
+        //  LocalDateTime time=LocalDateTime.now();
 
-Kullanici kullanici=new Kullanici(isim,LocalDateTime.now());
-kullaniciList.add(kullanici);
-return kullaniciList;
-
-    }
-public  void sansliKullanici(ArrayList<Kullanici>list) {
-    for (Kullanici w:list) {
-      if (w.kayitZamani.getSecond()<=10) {
-          System.out.println("Sn. "+w.name+" " +
-                  "bugun sansli gunundesin");
-      }else System.out.println("sansli gununde degilsin");
+        Kullanici kullanici = new Kullanici(isim, LocalDateTime.now());
+        kullaniciList.add(kullanici);
+        return kullaniciList;
 
     }
 
-}
-public void listele(){
-    System.out.println(kullaniciList);
+    public void sansliKullanici(ArrayList<Kullanici> list) {
+        for (Kullanici w : list) {
+            if (w.kayitZamani.getSecond() <= 10) {
+                System.out.println("Sn. " + w.name + " " +
+                        "bugun sansli gunundesin");
+            } else System.out.println("sansli gununde degilsin");
+
+        }
+
+    }
+
+    public void listele() {
+        System.out.println(kullaniciList);
 
 
-}
-public void cikis(){
-    System.out.println("gule gule");
-}
+    }
+
+    public void cikis() {
+        System.out.println("gule gule");
+    }
 
 }
