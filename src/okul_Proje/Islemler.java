4c0fce8e37dfd4a7dc7b6b9fdce044303f17c6fc
Mustafa Aba <mustafa_aba@outlook.com>
2023-07-17 11:54:46 +0300

    m.aba

--- src/okul_Proje/Islemler.java
+++ src/okul_Proje/Islemler.java
@@ -1,4 +1,39 @@
 package okul_Proje;
 
+import java.util.Scanner;
+
 public class Islemler {
+    Scanner input = new Scanner(System.in);
+
+    void anaMenu() {
+        System.out.println("====================================" +
+                "\nÖĞRENCİ VE ÖĞRETMEN YÖNETİM PANELİ" +
+                "\n\"====================================\"");
+        System.out.println("1- ÖĞRENCİ İŞLEMLERİ" + "\n2- ÖĞRETMEN İŞLEMLERİ" + "\nQ- ÇIKIŞ");
+        char anaMenuTercih = input.next().toUpperCase().charAt(0);
+        switch (anaMenuTercih) {
+            case '1':
+                //kisituru="ogrenci"
+                islemlerMenu();
+                break;
+            case '2':
+                //kisituru="ogretmen"
+                islemlerMenu();
+                break;
+
+            case 'Q':
+                //cikis();
+                break;
+            default:
+                System.out.println("Yanlış giriş yaptınız! Tekrar panele yönlendiriliyorsunuz.");
+                anaMenu();
+                break;
+        }
+    }
+
+    void islemlerMenu() {
+
+    }
+
+
 }
--- src/okul_Proje/Runner.java
+++ src/okul_Proje/Runner.java
@@ -3,5 +3,7 @@ package okul_Proje;
 public class Runner {
     public static void main(String[] args) {
 
+        Islemler islem=new Islemler();
+        islem.anaMenu();
     }
 }
