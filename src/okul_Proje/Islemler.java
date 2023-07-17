db93e7bde4f3f996b349147bbedfe76a97616df9
Mustafa Aba <mustafa_aba@outlook.com>
2023-07-17 12:06:18 +0300

    m.aba

--- src/okul_Proje/Islemler.java
+++ src/okul_Proje/Islemler.java
@@ -3,22 +3,24 @@ package okul_Proje;
 import java.util.Scanner;
 
 public class Islemler {
-  static Scanner input = new Scanner(System.in);
+    static Scanner input = new Scanner(System.in);
 
     void anaMenu() {
         System.out.println("====================================" +
                 "\nÖĞRENCİ VE ÖĞRETMEN YÖNETİM PANELİ" +
                 "\n\"====================================\"");
         System.out.println("1- ÖĞRENCİ İŞLEMLERİ" + "\n2- ÖĞRETMEN İŞLEMLERİ" + "\nQ- ÇIKIŞ");
-        char anaMenuTercih = input.next().charAt(0);
+        char anaMenuTercih = input.next().toUpperCase().charAt(0);
         switch (anaMenuTercih) {
             case '1':
+                //kisituru="ogrenci"
                 islemlerMenu();
                 break;
             case '2':
+                //kisituru="ogretmen"
                 islemlerMenu();
                 break;
-            case 'q':
+
             case 'Q':
                 //cikis();
                 break;
