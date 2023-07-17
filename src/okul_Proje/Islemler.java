cf4ee9a64cc16951a8ba143a5c9a1dad75bf8438
ZeynepA <zeyneppaytop@gmail.com>
2023-07-17 12:51:20 +0300

    islemler menu switch case added

--- src/okul_Proje/Islemler.java
+++ src/okul_Proje/Islemler.java
@@ -1,9 +1,15 @@
 package okul_Proje;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Islemler {
-    Scanner input = new Scanner(System.in);
+    static ArrayList<Kisi> ogretmenList = new ArrayList<>();
+    static ArrayList<Kisi> ogrenciList = new ArrayList<>();
+
+    static Scanner input = new Scanner(System.in);
+
+    static String kisiTuru= input.next();
 
     void anaMenu() {
         System.out.println("====================================" +
@@ -13,9 +19,11 @@ public class Islemler {
         char anaMenuTercih = input.next().charAt(0);
         switch (anaMenuTercih) {
             case '1':
+                kisiTuru= "ÖĞRENCİ";
                 islemlerMenu();
                 break;
             case '2':
+                kisiTuru = "ÖĞRETMEN";
                 islemlerMenu();
                 break;
             case 'q':
@@ -31,7 +39,40 @@ public class Islemler {
 
     void islemlerMenu() {
 
+        System.out.println("***** "+kisiTuru + " İŞLEMLERİ MENÜSÜNE HOŞ GELDİNİZ *****\nEKLEME işlemleri için 1\n"+
+                        "ARAMA işlemleri için 2\nLİSTELEME işlemleri için 3\nSİLME işlemleri için 4\nANA MENÜ'ye dönmek için 5\n" +
+                "ÇIKIŞ için Q seçiniz = ");
+
+        char islemlerMenuSecim = input.next().charAt(0);
+
+        switch (islemlerMenuSecim) {
+            case '1':
+              //  ekleme();
+                islemlerMenu();
+                break;
+            case '2':
+              //  arama();
+                islemlerMenu();
+                break;
+            case '3':
+               // listele();
+                islemlerMenu();
+                break;
+            case '4':
+               // silme();
+                islemlerMenu();
+                break;
+            case '5':
+                anaMenu();
+                break;
+            case 'q':
+            case 'Q':
+                //cikis();
+            default:
+                System.out.println("Yanlış bir seçim yaptınız, Lütfen tekrar deneyiniz...");
+                islemlerMenu();
+                break;
+        }
+
     }
-
-
 }
