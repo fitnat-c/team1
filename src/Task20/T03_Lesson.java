692050a018dc90c87b73d023058f22997d609d53
zerrin <zerrinizgu@gmail.com>
2023-07-18 14:55:24 +0200

    13 temmuz TASK

--- src/Task20/T03_Lesson.java
+++ src/Task20/T03_Lesson.java
@@ -1,5 +1,28 @@
 package Task20;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class T03_Lesson {
+
+String name;
+int credit;
+
+    public T03_Lesson(String name, int credit) {
+
+        this.name = name;
+        this.credit = credit;
+    }
+
+    @Override
+    public String toString() {
+        return "T03_Lesson{" +
+                "name='" + name + '\'' +
+                ", credit=" + credit +
+                '}';
+    }
+
+    static List<T03_Lesson> listDersler=new ArrayList<>();
+
 
 }
