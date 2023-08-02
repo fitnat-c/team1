5e968e77026beeced1b97a1d5ed2ef6cf16b0b7a
ZeynepA <zeyneppaytop@gmail.com>
2023-08-02 12:44:50 +0300

    Banka Hesabı project changes 02,08 added...

--- src/ZeynepA/bankaHesabı/Main.java
+++ src/ZeynepA/bankaHesabı/Main.java
@@ -2,32 +2,102 @@ package ZeynepA.bankaHesabı;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main extends Bank {
 
 
-    Scanner input = new Scanner(System.in);
+   public static Scanner input = new Scanner(System.in);
 
+    public Main(int hesapNumarasi, String hesapSahibi, int bakiye) {
+        super(hesapNumarasi, hesapSahibi, bakiye);
+    }
 
+    public static void anaMenu() {
+       System.out.println("********* Buddy Bank'a Hoşgeldiniz *********");
 
+       System.out.println("Müşteri işlemleri için 1'i\nBankamıza müşteri kaydı oluşturmak için 2'yi seçiniz: ");
+       int secimAnaMenu= input.nextInt();
 
+       if(secimAnaMenu==1){
 
-    //müşteri işlemleri menüsü
+           musteriIslemleri();
 
-   // var olan müşterinin işlemleri olacak... *hesapları listele *hesap aç, *kapat, *bakiye sorgula, para yatır, para çek, *kredi çekme...
+       }else if (secimAnaMenu==2){
 
-    //bakiye sorgulama ortak nokta.
+           bankaIslemleri();
 
-    ////////////////////////////////////////
+       }else System.out.println("yanlış tuşlama yaptınız lütfen tekrar deneyiniz...");
 
-    //banka işlemleri menüsü
+   }
 
-   // yeni müşteri kaydı, hesapları listele, *hesap açma, *kapat, *bakiye sorgulama, *kredi çekme.
+    static void musteriIslemleri(){
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n Buddy Bank Müşteri İşlemlerine Hoşgeldiniz\n"+
+                        "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ");
 
+        System.out.println("Değerli müşterimiz, Lütfen işleminizi seçiniz: \nBakiye Sorgulama 1 \nPara Yatırma 2 \n" +
+                "Para Çekme 3 \nKredi Çekme 4 \nHesaplarımı Listele 5 \nYeni Hesap Açma 6 \n Hesap Kapatma 7 \nAna Menü 8"+
+                "\nÇıkış 0");
 
+        int musteriIslemSecim= input.nextInt();
 
+        switch (musteriIslemSecim){
 
+            case 1:
+                bakiyeSorgula();
+                musteriIslemleri();
+                break;
+            case 2:
+                paraYatir();
+                musteriIslemleri();
+                break;
+            case 3:
+                paraCek();
+                musteriIslemleri();
+                break;
+            case 4:
+                krediCekme();
+                musteriIslemleri();
+                break;
+            case 5:
+                hesapListele();
+                musteriIslemleri();
+                break;
+            case 6:
+                hesapAc();
+                musteriIslemleri();
+                break;
+            case 7:
+                hesapKapat();
+                musteriIslemleri();
+                break;
+            case 8:
+                anaMenu();
+                break;
+            case 0:
+                cikis();
+                break;
+            default:
+                System.out.println("Yanlış giriş yaptınız, lütfen tekrar deneyiniz...");
+                break;
+        }
+    }
 
+static void bankaIslemleri(){
+    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n Buddy Bank Banka İşlemlerine Hoşgeldiniz\n"+
+            "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ");
+    System.out.println("Lütfen yapmak istediğiniz işlemi seçiniz: \nYeni Müşteri Kaydı 1 \nHesaplari Listeleme 2 \n"+
+            "Hesap Kontrolü 3");
+    int bankaIslemSecim= input.nextInt();
 
+    if (bankaIslemSecim==1){
 
+        hesapAc();
+    } else if (bankaIslemSecim==2) {
+        hesapListele();
+    } else if (bankaIslemSecim==3) {
+        hesapVarMi();
+    }else
+        System.out.println("yanlış giriş yaptınız...");
+
+    }
 
 }
--- src/ZeynepA/bankaHesabı/Runner.java
+++ src/ZeynepA/bankaHesabı/Runner.java
@@ -1,11 +1,12 @@
 package ZeynepA.bankaHesabı;
 
+import static ZeynepA.bankaHesabı.Main.anaMenu;
+
 public class Runner {
     public static void main(String[] args) {
 
-
-
-
-
+        anaMenu();
     }
 }
+
+
