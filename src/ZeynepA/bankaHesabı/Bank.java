271654000f360e54fa9d4510703484ba86cd23ad
Mustafa Aba <mustafa_aba@outlook.com>
2023-08-03 19:44:42 +0300

    m.aba 03.08

--- src/ZeynepA/bankaHesabı/Bank.java
+++ src/ZeynepA/bankaHesabı/Bank.java
@@ -8,7 +8,7 @@ import static ZeynepA.bankaHesabı.Main.input;
 
 public class Bank extends Account {
 
-   static ArrayList<Account> customerAccounts = new ArrayList<>();
+    static ArrayList<Account> customerAccounts = new ArrayList<>();
 
     public Bank() {
     }
@@ -17,53 +17,81 @@ public class Bank extends Account {
         super(hesapNumarasi, bakiye);
     }
 
-    static void hesapAc(){
+    static void hesapAc() {
         System.out.println("Lütfen adınızı, soy adınızı giriniz: ");
-        String customerName= new Scanner(System.in).nextLine();
-        Account cstmr1 = new Account(customerName);
+        String customerName = new Scanner(System.in).nextLine();
+
+        System.out.println("1-Vadeli Hesap\n2-Vadesiz Hesap");
+        boolean vadeliMi;
+        int vadeTercih=input.nextInt();
+        while (true) {
+            try {
+                if (vadeTercih == 1) {
+                    vadeliMi=true;
+                    break;
+                } else if (vadeTercih == 2) {
+                    vadeliMi=false;
+                    break;
+                } else
+                    throw new IllegalArgumentException("Hatalı giriş tekrar deneyiniz");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        Account cstmr1 = new Account(customerName,vadeliMi);
         customerAccounts.add(cstmr1);
         System.out.println("Yeni hesabınız oluşturulmuştur. iyi günlerde kullanın!");
 
     }
-    static void hesapKapat(){
+
+    static void hesapKapat() {
         System.out.println("Lütfen silmek istediğiniz hesap numarasini giriniz: ");
-        String silinecekHesap= new Scanner(System.in).nextLine();
-        if (customerAccounts.contains(silinecekHesap)){
+        String silinecekHesapNo = new Scanner(System.in).nextLine();
 
-            customerAccounts.remove(silinecekHesap);
-            System.out.println("hesabınız silinmiştir...");
-        }else System.out.println("Yanlış giriş yaptınız! Böyle bir hesap numarası sistemimizde bulunmamaktadır.");
+        for (Account hesap : customerAccounts) {
+            if ((hesap.getHesapNumarasi()).equals(silinecekHesapNo)) {
+                System.out.println("Hesap sahibi isim= " + hesap.getCustomerName());
+                customerAccounts.remove(hesap);
+                System.out.println("hesabınız silinmiştir...");
+            }else System.out.println("Yanlış giriş yaptınız! Böyle bir hesap numarası sistemimizde bulunmamaktadır.");
+
+        }
 
     }
 
-    static void bakiyeSorgula(){
 
 
-    }
-    static void hesapListele(){
+    static void hesapListele() {
 
         System.out.println("Hesap Listesi = " + customerAccounts);
     }
 
-    static void hesapVarMi(){
+    static Account hesapVarMi() {
+        System.out.println("Lütfen aramak istediğiniz hesap numarasini giriniz: ");
+        String arananHesapNo = new Scanner(System.in).nextLine();
 
         for (Account arananHesap : customerAccounts) {
-
-            System.out.println("arananHesap.getHesapNumarasi() = " + arananHesap.getCustomerName());
+            if ((arananHesap.getHesapNumarasi()).equals(arananHesapNo)) {
+                System.out.println("Hesap sahibi isim= " + arananHesap.getCustomerName());
+                return arananHesap;
+            }
         }
+        return null;
     }
 
-    static void krediCekme(){
+    static void krediCekme() {
         System.out.println("Lütfen çekmek istediğiniz kredi miktarını giriniz: ");
-        double krediTutari= input.nextInt();
-        double faizOrani = 1.95;
+        double krediTutari = input.nextInt();
+        System.out.println("Faiz oranını giriniz: ");
+        double faizOrani = input.nextDouble();
+        System.out.println("Kredi süresini ay olarak giriniz: ");
         int krediSuresi = input.nextInt();
 
-        System.out.println("Çektiğiniz krediyi "+krediSuresi+ " ay süresince geri ödeyebilirsiniz...");
-        System.out.println("Toplam geri ödemeniz "+krediFaiziHesapla(krediTutari,faizOrani,krediSuresi));
+        System.out.println("Çektiğiniz krediyi " + krediSuresi + " ay süresince geri ödeyebilirsiniz...");
+        System.out.println("Toplam geri ödemeniz " + krediFaiziHesapla(krediTutari, faizOrani, krediSuresi));
     }
 
-    static void cikis(){
+    static void cikis() {
         System.out.println("Bankamızdan çıkış yaptınız, iyi günler dileriz...");
     }
 
--- src/ZeynepA/bankaHesabı/Main.java
+++ src/ZeynepA/bankaHesabı/Main.java
@@ -5,69 +5,72 @@ import java.util.Scanner;
 public class Main extends Bank {
 
 
-   public static Scanner input = new Scanner(System.in);
+    public static Scanner input = new Scanner(System.in);
 
     public Main(String hesapNumarasi, int bakiye) {
         super(hesapNumarasi, bakiye);
     }
 
     public static void anaMenu() {
-       System.out.println("********* Buddy Bank'a Hoşgeldiniz *********");
+        System.out.println("********* Buddy Bank'a Hoşgeldiniz *********");
 
-       System.out.println("Müşteri işlemleri için 1'i\nBankamıza müşteri kaydı oluşturmak için 2'yi seçiniz: ");
-       int secimAnaMenu= input.nextInt();
+        System.out.println("Müşteri işlemleri için 1'i\nBankamıza müşteri kaydı oluşturmak için 2'yi seçiniz: ");
+        int secimAnaMenu = input.nextInt();
 
-       if(secimAnaMenu==1){
+        if (secimAnaMenu == 1) {
+            Account account = hesapVarMi();
+            assert account != null;
+            musteriIslemleri(account);
 
-           musteriIslemleri();
 
-       }else if (secimAnaMenu==2){
+        } else if (secimAnaMenu == 2) {
 
-           bankaIslemleri();
+            bankaIslemleri();
 
-       }else System.out.println("yanlış tuşlama yaptınız lütfen tekrar deneyiniz...");
+        } else System.out.println("yanlış tuşlama yaptınız lütfen tekrar deneyiniz...");
 
-   }
+    }
 
-    static void musteriIslemleri(){
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n Buddy Bank Müşteri İşlemlerine Hoşgeldiniz\n"+
-                        "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ");
+    static void musteriIslemleri(Account account) {
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n Buddy Bank Müşteri İşlemlerine Hoşgeldiniz\n" +
+                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ");
+
 
         System.out.println("Değerli müşterimiz, Lütfen işleminizi seçiniz: \nBakiye Sorgulama 1 \nPara Yatırma 2 \n" +
-                "Para Çekme 3 \nKredi Çekme 4 \nHesaplarımı Listele 5 \nYeni Hesap Açma 6 \n Hesap Kapatma 7 \nAna Menü 8"+
+                "Para Çekme 3 \nKredi Çekme 4 \nHesaplarımı Listele 5 \nYeni Hesap Açma 6 \n Hesap Kapatma 7 \nAna Menü 8" +
                 "\nÇıkış 0");
 
-        int musteriIslemSecim= input.nextInt();
+        int musteriIslemSecim = input.nextInt();
 
-        switch (musteriIslemSecim){
+        switch (musteriIslemSecim) {
 
             case 1:
-                bakiyeSorgula();
-                musteriIslemleri();
+                account.bakiyeSorgula();
+                musteriIslemleri(account);
                 break;
             case 2:
-                paraYatir();
-                musteriIslemleri();
+                account.paraYatir();
+                musteriIslemleri(account);
                 break;
             case 3:
-                paraCek();
-                musteriIslemleri();
+                account.paraCek();
+                musteriIslemleri(account);
                 break;
             case 4:
                 krediCekme();
-                musteriIslemleri();
+                musteriIslemleri(account);
                 break;
             case 5:
                 hesapListele();
-                musteriIslemleri();
+                musteriIslemleri(account);
                 break;
             case 6:
                 hesapAc();
-                musteriIslemleri();
+                musteriIslemleri(account);
                 break;
             case 7:
                 hesapKapat();
-                musteriIslemleri();
+                musteriIslemleri(account);
                 break;
             case 8:
                 anaMenu();
@@ -81,36 +84,36 @@ public class Main extends Bank {
         }
     }
 
-static void bankaIslemleri(){
-    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n Buddy Bank Banka İşlemlerine Hoşgeldiniz\n"+
-            "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ");
-    System.out.println("Lütfen yapmak istediğiniz işlemi seçiniz: \nYeni Müşteri Kaydı 1 \nHesaplari Listeleme 2 \n"+
-            "Hesap Kontrolü 3 \nAna Menüye 4 \nÇıkış 0");
-    int bankaIslemSecim= input.nextInt();
+    static void bankaIslemleri() {
 
-    switch (bankaIslemSecim){
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n Buddy Bank Banka İşlemlerine Hoşgeldiniz\n" +
+                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ");
+        System.out.println("Lütfen yapmak istediğiniz işlemi seçiniz: \nYeni Müşteri Kaydı 1 \nHesaplari Listeleme 2 \n" +
+                "Hesap Kontrolü 3 \nAna Menüye 4 \nÇıkış 0");
+        int bankaIslemSecim = input.nextInt();
 
-        case 1:
-            hesapAc();
-            bankaIslemleri();
-            break;
-        case 2:
-            hesapListele();
-            bankaIslemleri();
-            break;
-        case 3:
-            hesapVarMi();
-            bankaIslemleri();
-            break;
-        case 4:
-            anaMenu();
-            break;
-        case 0:
-            cikis();
-            break;
-        default:
-            System.out.println("yanlış giriş yaptınız...");
-            break;
-    }
+        switch (bankaIslemSecim) {
+            case 1:
+                hesapAc();
+                bankaIslemleri();
+                break;
+            case 2:
+                hesapListele();
+                bankaIslemleri();
+                break;
+            case 3:
+                hesapVarMi();
+                bankaIslemleri();
+                break;
+            case 4:
+                anaMenu();
+                break;
+            case 0:
+                cikis();
+                break;
+            default:
+                System.out.println("yanlış giriş yaptınız...");
+                break;
+        }
     }
 }
